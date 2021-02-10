623e5612a2bd9e53d4bc9721e4b5dd5edd3e5fd2
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-02-10 15:41:03 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
+import AboutUs from './components/pages/AboutUs'
+import AboutEachPerson from './components/pages/AboutEachPerson'
+import NavBar from './components/pages/NavBar'
 
 let team = [
   {name: 'Jackson', bio: 'From Austin Texas'},
@@ -8,10 +12,14 @@ let team = [
   {name: 'Patrick', bio: 'From Palm Springs California'}
 ]
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <Home team={team} />
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Route path = '/aboutus' component={AboutUs} />
+      <Route path = '/about:name' render={(props) => <AboutEachPerson {...props} team={team} />} />
+
     </div>
   );
 }
