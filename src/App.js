aa8a4dd0c4d92c7000977ca33f7d8bfe83ea61eb
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-02-10 15:27:37 -0600

    team array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Home from './components/pages/Home'
 
+let team = [
+  {name: 'Jackson', bio: 'From Austin Texas'},
+  {name: 'Trisha', bio: 'From LA California'},
+  {name: 'Blake', bio: 'From Las Vegas Nevada'},
+  {name: 'Patrick', bio: 'From Palm Springs California'}
+]
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home team={team} />
     </div>
   );
 }
